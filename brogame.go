e66b6de5fcb10ccf5969d1bfa175fa97de4df980
Daniel Marshall <Dmars8047@live.com>
2023-03-05 23:33:01 -0500

    added scene, context, and gameobject

--- brogame.go
+++ brogame.go
@@ -3,4 +3,7 @@ package brogame
 import (
 	_ "github.com/dmars8047/brogame/animation"
 	_ "github.com/dmars8047/brogame/asset"
+	_ "github.com/dmars8047/brogame/context"
+	_ "github.com/dmars8047/brogame/gameobject"
+	_ "github.com/dmars8047/brogame/scene"
 )
